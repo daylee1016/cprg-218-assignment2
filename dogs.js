46fbf90b43584af5221a41ef72800d87f845cef3
daylee1016 <dayeong.lee@edu.sait.ca>
2024-11-26 02:14:14 -0700

    figcaption edited - js
    figcaption in js for each dogs are added.

--- dogs.js
+++ dogs.js
@@ -2,7 +2,7 @@ about.addEventListener('click',function(){
     console.log('Read about Tori and Dianne!');
     document.getElementById('img').src='images/logo.png';
     h2.innerHTML = "About T&D Studio";
-    figcaption.innerHTML = "T&D is named after Dianne and her dog, Tori. Dianne painted wild animals before. After meeting Tori, she decided to paint for pets for pet parents to capture a special moment for both pet parents and their beloved furry friends."
+    figcaption.innerHTML = "T&D is named after Dianne and her dog, Tori. Dianne painted only wild animals using acrylics. However, after meeting Tori, she decided to share her talen by creating pet portraits to capture special and memorable moemnts for pet parent's beloved furry friends. She now specializes in digital art."
 
 })
 
@@ -10,7 +10,7 @@ tori.addEventListener('click',function(){
     console.log('Say Hi to Tori!');
     document.getElementById('img').src='images/tori.png';
     h2.innerHTML = "Meet Tori!";
-    figcaption.innerHTML = "On April 18th, 2024, Dianne drew her dog Tori."
+    figcaption.innerHTML = "On April 18th, 2024, Dianne drew her dog, Tori. He is a Pomeranian mix and loves snow. He was born in Manitoba in 2021. He has been Dianne's furever best friend ever since. Although he is a crazy and naughty dog who loves to chew on the family's AirPods and shoes, he is the best and sweetest companion she could ask for."
 
 })
 
@@ -18,14 +18,12 @@ ponzu.addEventListener('click',function(){
     console.log('This is Ponzu, the Samoyed.');
     document.getElementById('img').src='images/Ponzu.png';
     h2.innerHTML = "Meet Ponzu!";
-    figcaption.innerHTML = "On July 8th, 2024, Dianne drew Ponzu the Samoyed."
-
+    figcaption.innerHTML = "On July 6th, 2024, Dianne drew a master piece of a fluffy Samoyed named Ponzu. Ponzu is name after a citrus sauce commonly used in Japan. She met her furever family in Calgary, and her story of adventure continues non-stop. Her favourite season of the year us winter and she is having her best life in Canada, where she can enjoy endless snowy days with her humans."
 })
 
 eddie.addEventListener('click',function(){
     console.log('Mini Schanuzer, Mr.Eddie');
     document.getElementById('img').src='images/Eddie.png';
     h2.innerHTML = "Meet Eddie!";
-    figcaption.innerHTML = "On August 9th, 2024, Dianne drew Eddie the Mini Schanuzer."
-
+    figcaption.innerHTML = "On August 9th, 2024, Dianne drew a Mini Schnauzer named Eddie. His shiny silver coat and distinctive eyebrows make him unforgettable to anyone who sees him. This good boy, Eddie, is from Toronto and is living the best life with his pet parents. He is small but full of energy. Always egear to show affection and spreading joy to the people around him."
 })
